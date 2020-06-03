2149beeec71c4ad1075d9387b28f03dec6158b58
Fongwith87592 <fongwith87592@gmail.com>
2020-06-03 11:24:02 +0800

    test

---
+++ src/main/hw3_2.java
@@ -0,0 +1,11 @@
+package main;
+
+public class hw3_2 {
+	public static void main(String args[]) {
+		AbstractXmasTree XmasTree1 = new XmasTree();
+		XmasTree1.sing();
+		
+		AbstractXmasTree XmasTree2 = new Gift(new Bell(new Candy(XmasTree1)));
+		XmasTree2.sing();
+	}
+}
